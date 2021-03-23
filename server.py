1f77a61ad6a152fcddca89c2442b178e3077d421
Beka00best <bukhatovb@mail.ru>
2021-03-24 04:37:12 +0600

    server

--- server.py
+++ server.py
@@ -49,5 +49,5 @@ def download(folder, filename):
     return render_template("index.html", args=args)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
